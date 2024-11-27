af7f46b768e1d479ceca8ee51b5c72307cad3277
Mariane Teles <marianebteles2021@hotmail.com>
2024-11-27 14:49:52 -0300

    Commit004

--- SistemaBarcos.UI/Models/Cadastro.cs
+++ SistemaBarcos.UI/Models/Cadastro.cs
@@ -9,15 +9,18 @@ namespace SistemaBarcos.UI.Models
 {
     public class Cadastro : Validacao
     {
-
+        
         public int CadastrarDados(string table, string d1, string d2, string d3)
         {
-            string connectionString = "Data Source=FAC0539673W10-1;Initial Catalog=M34Db;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string connectionString = "Data Source=DESKTOP-B2J8C6H\\SQLEXPRESS;Initial Catalog=M34Dd;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            /*Chave senac: Data Source=FAC0539673W10-1;Initial Catalog=M34Db;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False*/
 
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
 
             SqlCommand inserirCommand = new SqlCommand($"insert into {table} values(@d1, @d2, @d3); ", connection);
+
+            /*SqlCommand GetIdCommand = new SqlCommand($"Select * {table} values(@d1, @d2, @d3); ", connection);*/
 
             inserirCommand.Parameters.AddWithValue("@d1", d1);
             inserirCommand.Parameters.AddWithValue("@d2", d2);
